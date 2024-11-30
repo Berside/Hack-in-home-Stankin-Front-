cd9c0b586aedcf70a061e727b8ac1db71eb1248c
Ilya <berside@bk.ru>
2024-11-30 22:37:31 +0300

    fetch

---
+++ src/utils/post.jsx
@@ -0,0 +1,20 @@
+import endpoints from './endpoint.jsx';
+const baseUrl = endpoints();
+
+export async function post(formdata) {
+  try {
+    const response = await fetch(`${baseUrl}/api/v1/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: formdata
+    });
+    const result = await response.json();
+    console.log('Success:', result);
+    return result;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
